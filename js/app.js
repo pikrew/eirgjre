4e8314936bcb7bc341a13dd97a7e5ef15211c4cc
pikrew <167602020+pikrew@users.noreply.github.com>
2024-04-24 11:25:36 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const burger = () => {
+    const btn = document.querySelector(',burger svg:last-child')
+    const off = document.querySelector(',offset')
+    const close = document.querySelector('.offs')
+    btn.addEventListener('click', () => {
+        
+    })
+}
